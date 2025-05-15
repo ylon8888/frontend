bbea72030158e54f5f1b9886d2b7f03705d6361e
moriyam-mohona <moriyammohona@gmail.com>
2025-05-15 10:11:59 +0600

    Step Two Created

--- src/components/singleChapter/ChapterLayout.tsx
+++ src/components/singleChapter/ChapterLayout.tsx
@@ -1,12 +1,14 @@
 import React from "react";
-import StepOne from "./chapterContents/StepOne";
+// import StepOne from "./chapterContents/StepOne";
 import Steps from "./Steps";
+import StepTwo from "./chapterContents/StepTwo";
 
 const ChapterLayout = () => {
   return (
     <div className="flex gap-10 w-full my-16 md:my-24">
       <div className="w-full lg:w-2/3">
-        <StepOne />
+        {/* <StepOne /> */}
+        <StepTwo />
       </div>
       <div className="w-full lg:w-1/3 border shadow-lg p-5 rounded-2xl">
         <Steps />
