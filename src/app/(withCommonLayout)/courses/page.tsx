a0af144e99e3e61f48308c27e4c3df00648d5df1
moriyam-mohona <moriyammohona@gmail.com>
2025-06-30 11:37:38 +0600

    AuthGuad added on course

--- src/app/(withCommonLayout)/courses/page.tsx
+++ src/app/(withCommonLayout)/courses/page.tsx
@@ -4,18 +4,21 @@ import ExpectfromPodcastClasses from "@/components/home/ExpectfromPodcastClasses
 import Faq from "@/components/home/Faq";
 import SuccessInAcademicYear from "@/components/home/SuccessInAcademicYear";
 import Testimonials from "@/components/shared/Testimonials/Testimonials";
+import WithRole from "@/role-wrapper/WithAdmin";
 import React from "react";
 
 const Courses = () => {
   return (
-    <div>
-      <CoursesHero />
-      <ExpectfromPodcastClasses />
-      <CoursesPage />
-      <Testimonials />
-      <Faq />
-      <SuccessInAcademicYear />
-    </div>
+    <WithRole allowedRoles={["STUDENT"]}>
+      <div>
+        <CoursesHero />
+        <ExpectfromPodcastClasses />
+        <CoursesPage />
+        <Testimonials />
+        <Faq />
+        <SuccessInAcademicYear />
+      </div>
+    </WithRole>
   );
 };
 
